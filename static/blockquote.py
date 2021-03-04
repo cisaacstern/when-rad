ea97d77be0ff227494ce5eb168872f397ee4f064
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2021-03-04 01:33:55 -0800

    Update blockquote.py

--- static/blockquote.py
+++ static/blockquote.py
@@ -1,4 +1,5 @@
 blockquote = """The web interface for this project is a work in progress! 
-For a reference of what this modules output will eventually look like, please 
-refer to https://twitter.com/charlesstern/status/1357018916246016000?s=20 .
+For a reference of what this app's output will eventually look like, please 
+refer to <a href="https://twitter.com/charlesstern/status/1357018916246016000?s=20">
+this Tweet</a>.
 """
