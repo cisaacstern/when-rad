48faf7adba882c28538d2ace8b2c44f2b287e63f
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2021-03-04 01:26:16 -0800

    Update blockquote.py

--- static/blockquote.py
+++ static/blockquote.py
@@ -1,2 +1,4 @@
-blockquote = """A bit more detail.
+blockquote = """The web interface for this project is a work in progress! 
+For a reference of what this modules output will eventually look like, please 
+refer to https://twitter.com/charlesstern/status/1357018916246016000?s=20 .
 """
